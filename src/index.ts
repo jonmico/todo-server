c6a7996dd91c6f16df4969659250d3468fb423e9
Jon Mico <jonmico@gmail.com>
2026-08-01 18:10:43 -0400

    Adds connection.ts.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import express from "express";
 import { errorHandler } from "./middleware/errorHandler";
+
+dotenv.config({ quiet: true });
 
 const app = express();
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("App is listening on port 3000.");
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on port ${process.env.PORT}.`);
 });
