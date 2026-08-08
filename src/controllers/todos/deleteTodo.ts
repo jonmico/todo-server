51165536502c2e0431786e11464c6e3fd25b5505
Jon Mico <jonmico@gmail.com>
2026-08-08 08:24:47 -0400

    Finishes deleteTodo.

--- src/controllers/todos/deleteTodo.ts
+++ src/controllers/todos/deleteTodo.ts
@@ -1,5 +1,22 @@
 import type { Request, Response } from "express";
+import { pool } from "../../db/connection";
+import { ResultSetHeader } from "mysql2";
+import { AppError } from "../../utils/AppError";
 
 export async function deleteTodo(req: Request, res: Response) {
-  res.json({ message: "You are in the deleteTodo controller!" });
+  const { id } = req.params;
+
+  const [result] = await pool.query<ResultSetHeader>(
+    `
+      DELETE FROM todos
+      WHERE id = ? AND user_id = ?;
+    `,
+    [id, req.userId],
+  );
+
+  if (result.affectedRows === 0) {
+    throw new AppError(404, "Todo not found.");
+  }
+
+  res.status(204).end();
 }
