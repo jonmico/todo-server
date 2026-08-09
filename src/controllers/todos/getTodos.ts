ba75adcdcc243616931fde51a768081e85ba67cc
Jon Mico <jonmico@gmail.com>
2026-08-09 11:43:47 -0400

    Alright so we scrapped the tags idea for now.

--- src/controllers/todos/getTodos.ts
+++ src/controllers/todos/getTodos.ts
@@ -1,35 +1,47 @@
 import type { Request, Response } from "express";
 import { RowDataPacket } from "mysql2";
 import { pool } from "../../db/connection";
+import { Todo } from "../../types/todo";
 
-interface TodoTagRow extends RowDataPacket {
-  todo_id: string;
-  user_id: string;
-  title: string;
-  description: string | null;
+// interface TodoTagRow extends RowDataPacket {
+//   todo_id: string;
+//   user_id: string;
+//   title: string;
+//   description: string | null;
+//   completed: number;
+//   due_date: Date | null;
+//   created_at: Date;
+//   updated_at: Date;
+//   tag_id: string | null;
+//   tag_name: string | null;
+// }
+
+interface TodoRow extends RowDataPacket, Todo {
   completed: number;
-  due_date: Date | null;
-  created_at: Date;
-  updated_at: Date;
-  tag_id: string | null;
-  tag_name: string | null;
 }
 
 export async function getTodos(req: Request, res: Response) {
-  const [todos] = await pool.query<TodoTagRow[]>(
+  // const [todos] = await pool.query<TodoTagRow[]>(
+  //   `
+  //     SELECT todo.id AS todo_id, todo.title, todo.description, todo.completed,
+  //       todo.due_date, todo.created_at, todo.updated_at,
+  //       tag.id AS tag_id, tag.name AS tag_name
+  //     FROM todos todo
+  //     LEFT JOIN todo_tags tt ON tt.todo_id = todo.id
+  //     LEFT JOIN tags tag ON tag.id = tt.tag_id
+  //     WHERE todo.user_id = ?
+  //     `,
+  //   [req.userId],
+  // );
+
+  const [todos] = await pool.query<TodoRow[]>(
     `
-      SELECT todo.id AS todo_id, todo.title, todo.description, todo.completed, 
-        todo.due_date, todo.created_at, todo.updated_at,
-        tag.id AS tag_id, tag.name AS tag_name
-      FROM todos todo
-      LEFT JOIN todo_tags tt ON tt.todo_id = todo.id
-      LEFT JOIN tags tag ON tag.id = tt.tag_id
-      WHERE todo.user_id = ?  
-      `,
+      SELECT *
+      FROM todos
+      WHERE user_id = ?
+    `,
     [req.userId],
   );
-
-  console.log(todos);
 
   res.json({ todos });
 }
